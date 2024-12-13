4a7d0e08bd0172206e32b69ee1e62b23b398ca75
Saranya Hiransatakun <saranya.hi@easysunday.com>
2024-12-13 22:03:33 +0700

    part 2

--- day4/main.go
+++ day4/main.go
@@ -10,6 +10,10 @@ var STRING = "XMAS"
 var LEN_STRING = len(STRING)
 var DIRECTION = [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
 
+func is_out_of_range(x int, y int, len_row int, len_col int) bool {
+	return x < 0 || y < 0 || x >= len_row || y >= len_col
+}
+
 func partOne(table [][]string) {
 	count := 0
 	for row_i, row := range table {
@@ -24,8 +28,7 @@ func partOne(table [][]string) {
 				for i, s := range STRING {
 					focus_x := row_i + i*dir[0]
 					focus_y := col_i + i*dir[1]
-					out_of_range := focus_x < 0 || focus_y < 0 || focus_x >= len(row) || focus_y >= len(table)
-					if out_of_range || table[focus_x][focus_y] != string(s) {
+					if is_out_of_range(focus_x, focus_y, len(row), len(table)) || table[focus_x][focus_y] != string(s) {
 						break
 					}
 					if table[focus_x][focus_y] == "S" {
@@ -35,6 +38,43 @@ func partOne(table [][]string) {
 				if found {
 					count += 1
 				}
+			}
+		}
+	}
+	fmt.Printf("answer: %d\n", count)
+}
+
+var DIRECTION_2 = [][]int{{-1, -1}, {-1, 1}}
+
+// https://adventofcode.com/2024/day/4#part2
+func partTwo(table [][]string) {
+	count := 0
+	for row_i, row := range table {
+		for col_i, v := range row {
+			// focus only A
+			if v != "A" {
+				continue
+			}
+			// find a valid direction
+			count_word := 0
+			for _, dir := range DIRECTION_2 {
+				focus_x := row_i + dir[0]
+				focus_y := col_i + dir[1]
+				op_x := row_i - dir[0]
+				op_y := col_i - dir[1]
+				out_of_range := is_out_of_range(focus_x, focus_y, len(row), len(table)) || is_out_of_range(op_x, op_y, len(row), len(table))
+				is_x_mas := false
+				if !out_of_range {
+					is_x_mas = strings.Contains("MS", table[focus_x][focus_y]) && strings.Contains("MS", table[op_x][op_y])
+				}
+				if out_of_range || table[focus_x][focus_y] == table[op_x][op_y] || !is_x_mas {
+					break
+				}
+				count_word += 1
+			}
+
+			if count_word == 2 {
+				count += 1
 			}
 		}
 	}
@@ -66,6 +106,6 @@ func main() {
 	table := formatInput(readFile("input.txt"))
 
 	// start
-	partOne(table)
+	partTwo(table)
 
 }
